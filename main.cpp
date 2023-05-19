df53ed6cac44b89830028323006c27312192c9a2
sarahishamsaied <sarahishamsaied@gmail.com>
2023-05-19 20:46:36 +0300

    bugfix: created one obstacle

--- main.cpp
+++ main.cpp
@@ -35,34 +35,20 @@ int logWidth=100;
 int logHeight=100;
 int limit = 110;
 float mouseX;
-float randX;
+float randX=20;
 float mouseY;
 Triangle cone;
 TriangleViewer coneView(cone);
 Rocket rocket;
 RocketViewer rocketViewer;
 std::unordered_set<int> generatedCoordinates;
-std::vector<float> obstaclePositions;  // Stores the Y positions of the obstacles
-int generateUniqueRandomCoordinates(float& x) {
-    default_random_engine generator;
-    uniform_int_distribution<int> distribution(20, 90);
-    x = distribution(generator);
-    cout << "randomX" << x << endl;
-    return x;
+int generateUniqueRandomCoordinates(float &x) {
+   default_random_engine generator;
+   uniform_int_distribution <int> distribution(20,90);
+   x = distribution(generator);
+   cout<<"randomX"<<x<<endl;
+   return x;
 }
-void generateObstacles() {
-    generatedCoordinates.clear();
-    obstaclePositions.clear();
-
-    for (int i = 0; i < 7; i++) {
-        float randomX;
-        int randomCoord = generateUniqueRandomCoordinates(randomX);
-        generatedCoordinates.insert(randomCoord);
-        obstaclePositions.push_back(limit - i);
-    }
-}
-
-
  void drawCircle(float cx, float cy, float radius, int segments) {
      glColor3f(.1,.3,.4);
         glBegin(GL_POLYGON);
@@ -75,23 +61,20 @@ void generateObstacles() {
         glEnd();
 }
 void Timer(int value){
-    glutTimerFunc(70, Timer, value);
+    glutTimerFunc(30, Timer, value);
 glutPostRedisplay();
 }
-void display() {
+void display(){
     glClear(GL_COLOR_BUFFER_BIT);
     rocketViewer.draw();
-    for (size_t i = 0; i < obstaclePositions.size(); i++) {
-        drawCircle(obstaclePositions[i], obstaclePositions[i], 5, 100);
-        obstaclePositions[i]--;
-        if (obstaclePositions[i] <= -6) {
-            obstaclePositions[i] = 110;
-        }
+    drawCircle(randX, limit, 5, 100);
+    --limit;
+    if(limit <=-6){
+        limit = 110;
+        randX = 20 + rand()%90;
     }
-
-    cout << "randomX is " << randX;
-
-    int x, y;
+    cout<<"randomX is"<<randX;
+    int x,y;
     glutSwapBuffers();
 }
 void keyboardHandler(unsigned char key, int x, int y){
@@ -107,7 +90,6 @@ void mouseHandler(int x, int y){
     cout<<"x: "<<mouseX<<endl;
     cout<<mouseY<<endl;
     rocketViewer.moveRocket(mouseX, mouseY);
-        cout<<"fff:";
     cout<<rocketViewer.getRocket().getBody().getVertices()[0].first<<endl;
     glutPostRedisplay();
 }
@@ -121,7 +103,6 @@ gluOrtho2D( 0.0, logWidth, 0.0, logHeight);
 }
 int main(int argc, char *argv[])
 {
-    generateObstacles();
     glutInit(&argc, argv);
     glutInitWindowSize(phyWidth,phyHeight);
     glutInitWindowPosition(10,10);
@@ -133,6 +114,5 @@ int main(int argc, char *argv[])
     glutPassiveMotionFunc(mouseHandler);
     Timer(0);
     glutMainLoop();
-
     return EXIT_SUCCESS;
 }
